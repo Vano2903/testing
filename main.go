507155ee19e6003f857c90d3ee4bf70a72934f26
davide vanoncini <39943465+Vano2903@users.noreply.github.com>
2022-06-13 14:53:16 +0200

    7 versions are a bit too much

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello from the updated ipaas container :D v6.0 😭😭")
+		fmt.Fprintf(w, "hello from the updated ipaas container :D v7.0 😭😭")
 	})
 
 	log.Println("starting on port 8080")
