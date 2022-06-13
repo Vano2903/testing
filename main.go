fa662a380de7546fba79ba9def16607e2de2f995
davide vanoncini <39943465+Vano2903@users.noreply.github.com>
2022-06-13 14:51:48 +0200

    v6
    😭

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello from the updated ipaas container :D v5.0 😭")
+		fmt.Fprintf(w, "hello from the updated ipaas container :D v6.0 😭😭")
 	})
 
 	log.Println("starting on port 8080")
