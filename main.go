e43fbcf832690247aabe81bf96ca19a6506a1df2
vano <davidevanoncini2003@gmail.com>
2022-07-07 22:07:09 +0200

    build but dies with runtime error

--- main.go
+++ main.go
@@ -4,12 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	var error string = 1
+	os.Exit(1)
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello from the updated ipaas container :D v9.1 (sheesh version)")
