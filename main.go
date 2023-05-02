c6761ce85209df4ea1aaf9eea4faf937f93b1a50
davide vanoncini <39943465+Vano2903@users.noreply.github.com>
2023-05-02 16:59:45 +0200

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello from the updated ipaas container :D v9.1 (sheesh version)")
+		fmt.Fprintf(w, "hello from the updated ipaas container :D v9.2 (sheesh version) (big update)")
 	})
 
 	log.Println("starting on port 8080")
