1bcc617b110dd9c06ab1fdc0776c65be2290a81d
davide vanoncini <39943465+Vano2903@users.noreply.github.com>
2023-11-20 10:24:06 +0100

    add hi endpoint
    this endpoint is to check if endpoints are forwarded correctly

--- main.go
+++ main.go
@@ -13,7 +13,9 @@ func main() {
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello from the updated ipaas container :D v9.3 (sheesh version) (big update)")
 	})
-
+	r.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hi, this is the request from an endpoint")
+	})
 	log.Println("starting on port 8080")
 	log.Fatalln(http.ListenAndServe(":8080", r))
 }
